172d4340b059788df1bd694c06208046eb81ceaa
xrrbx <xrrbx@protonmail.com>
2026-06-22 11:16:40 +0200

    macros practice

--- models/0_staging/stripe/stg_stripe__payments.sql
+++ models/0_staging/stripe/stg_stripe__payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid AS order_id,
     paymentmethod as payment_method,
-    amount / 100 AS amount,
+    {{ cents_to_dollars("amount",4) }} AS amount,
     status,
     created as created_at
 from {{source('stripe', 'payment')}}
