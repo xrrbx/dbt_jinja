47491ff60a3adabb69c623a55f8c55c27a947071
xrrbx <xrrbx@protonmail.com>
2026-06-25 11:20:12 +0200

    macros exercises

---
+++ macros/grant_select.sql
@@ -0,0 +1,11 @@
+{% macro grant_select( schema=target.schema, role=target.role, database=target.database ) %}
+    {% set sql %}
+        grant usage on schema {{ schema }} to role {{ role }};
+        grant select on all tables in schema {{ schema }} to role {{ role }};
+        grant select on all views in schema {{ schema }} to role {{ role }}
+    {% endset %}
+
+    {{ log ('Granting select on schema ' ~ schema ~ ' to role ' ~ role, info=True) }}
+    {% do run_query(sql) %}
+    {{ log ('Finished granting select on schema ' ~ schema ~ ' to role ' ~ role, info=True) }}
+{% endmacro %}
---
+++ models/scratch.sql
@@ -0,0 +1,5 @@
+{{ union_tables_by_prefix }}
+{{ target.role }}
+{{ target.schema }}
+
+select * from raw.dbt_learn_jinja.orders__shopify
